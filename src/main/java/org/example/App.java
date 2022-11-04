76cdd79598fb4a73a64017f620c9abe3c62493c3
MatteoDZ <mttdlz97@gmail.com>
2022-11-04 17:08:23 +0100

    Aggiunto popolatore per i customer, il manager e i prodotti

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,15 @@
 package org.example;
+import net.datafaker.Faker;
 import org.json.JSONObject;
 import redis.clients.jedis.*;
 import org.example.Classes.*;
 
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.Date;
 import java.util.*;
+
+import static java.lang.Boolean.FALSE;
 
 public class App 
 {
@@ -81,6 +87,9 @@ public class App
         }
         pool.close();
         */
+
+        populate();
+
         System.out.println("Benvenuto in Ecommerce!");
         while (connectionState == -1) {
             int mode = cManager.FirstAccessPossibleCommands();
@@ -127,6 +136,82 @@ public class App
                 break;
             default:
                 System.out.println("Error in the system.");
+        }
+
+    }
+
+    public static void populate(){
+        Faker faker = new Faker(new Locale("it"));
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce", "root", "password")) {
+
+            String delCus = "DELETE FROM customer";
+            PreparedStatement delCusSt = connection.prepareStatement(delCus);
+            delCusSt.executeUpdate();
+
+            String delMan = "DELETE FROM manager";
+            PreparedStatement delManSt = connection.prepareStatement(delMan);
+            delManSt.executeUpdate();
+
+            String delPro = "DELETE FROM product";
+            PreparedStatement delProSt = connection.prepareStatement(delPro);
+            delProSt.executeUpdate();
+
+            String sqlman = "INSERT INTO manager SET Name = 'Manager', Surname = 'Direttore', Email = 'ilcapo@email.it', Password = 'password', Phonenumber = '0123456789', Company = 'Unipi'";
+            PreparedStatement pstman = connection.prepareStatement(sqlman);
+            pstman.executeUpdate();
+
+            for (int i = 0; i < 1000; i++) {
+                String sql = "INSERT INTO customer SET Name = ?, Surname = ?, Email = ?, Password = ?, Phonenumber = ?, Active = 1, RegistrationDate = ?, Address = ?, Country = ?";
+                PreparedStatement pst = connection.prepareStatement(sql);
+
+                String firstName = faker.name().firstName();
+                String lastName = faker.name().lastName();
+                String email = firstName.toLowerCase() + lastName.toLowerCase() + faker.number().numberBetween(01, 99) + "@email.it";
+                String password = faker.password().toString();
+                String phoneNumber = faker.phoneNumber().phoneNumber();
+                Date date = new Date(122, 10, 2);
+                String address = faker.address().streetAddress(FALSE) + " , " + faker.address().city() + " " + faker.address().zipCode();
+
+                pst.setString(1, firstName);
+                pst.setString(2, lastName);
+                pst.setString(3, email);
+                pst.setString(4, password);
+                pst.setString(5, phoneNumber);
+                pst.setDate(6, date);
+                pst.setString(7, address);
+                pst.setString(8, "Italy");
+
+                pst.executeUpdate();
+                System.out.println("Ho completato " + i + " operazioni");
+            }
+
+            for (int i = 0; i < 100; i++) {
+                String sql = "INSERT INTO product SET Name = ?, ShortDescription = ?, Description = ?, Brand = ?, ImageUrl = ?, Price = ?, Stock = ?";
+                PreparedStatement pst = connection.prepareStatement(sql);
+
+                String name = faker.vehicle().makeAndModel();
+                String shDesc = faker.lordOfTheRings().character();
+                String desc = faker.lordOfTheRings().location();
+                String[] splited = name.split("\\s+");
+                String brand = splited[0];
+                String img = "";
+                double price = faker.number().randomDouble(3, 10000, 200000);
+                Integer stock = faker.number().numberBetween(1, 10);
+
+                pst.setString(1, name);
+                pst.setString(2, shDesc);
+                pst.setString(3, desc);
+                pst.setString(4, brand);
+                pst.setString(5, img);
+                pst.setFloat(6, (float) price);
+                pst.setInt(7, stock);
+
+                pst.executeUpdate();
+                System.out.println("Ho completato " + i + " operazioni");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
